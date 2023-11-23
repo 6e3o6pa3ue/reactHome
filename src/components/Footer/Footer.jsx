7f95862cd06bb184c76a4885011a0faabebc0f19
6e3o6pa3ue <alexeyprivalov3@gmail.com>
2023-11-23 17:27:15 +0100

    Окончательный файл

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,67 +1,52 @@
-import * as React from 'react';
-import "../../Footer.css";
-import FacebookIcon from '@mui/icons-material/Facebook';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GoogleIcon from '@mui/icons-material/Google';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import  '../../Footer.css'
 
+const Footer = () =>{
+    return(
 
-export default function Footer(props) {
-
-    return (
-        <footer>
-            <div className="another-background">
-                <div className="rights">
-                    <div className="add-info">
-                        <p className="add-info-item">Terms of Use</p>
-                        <p className="add-info-item">Privisy-Policy</p>
-                        <p className="add-info-item">FAQ</p>
-                        <p className="add-info-item">Watch List</p>
-                    </div>
-                    <div className="copyrights">
-                        <p className="copyrights-text">© 2023 WATCHIT. All Rights Reserved. All videos and shows on this platform are trademarks of, and all related images and content are the property of Streamit Inc. Duplication and copy of this is strictly prohibited. All rights reserved. </p>
-                    </div>
-                </div>
-                <div className="followUs">
-                    <p className='text-follow'>Follow Us:</p>
-                    <div className="icons">
-                        <div className="icon">
-                            <FacebookIcon className='imgIcon'/>
-                        </div>
-                        <div className="icon">
-                            <TwitterIcon className='imgIcon'/>
-                        </div>
-                        <div className="icon">
-                            <GoogleIcon className='imgIcon'/>
-                        </div>
-                        <div className="icon">
-                            <GitHubIcon className='imgIcon'/>
-                        </div>
-                    </div>
-                </div>
-                <div className="watchItApp">
-                    <p className="thisApp">
-                        Watchit App
-                    </p>
-                    <div className="stores">
-                        <div className="google__store store">
-                            <p className='comment'>Get it on</p>
-                            <div className="store">
-                                <img src="https://cdn-icons-png.flaticon.com/128/6124/6124997.png" alt="#" className='icon-store' />
-                                <p className="store-name">Google Play</p>
-                            </div>
-                        </div>
-                        <div className="apple__store">
-                            <p className='comment'>Download on the</p>
-                            <div className="store">
-                                <img src="https://cdn-icons-png.flaticon.com/128/179/179309.png" alt="#" className='icon-store' />
-                                <p className="store-name">App Store</p>
-                            </div>
-                        </div>
-                    </div>
-                    
-                </div>
+     <div className='footer'>
+        <div className='conteiner__1'>
+            <div className='span_section'>
+            <h1 className='text__style'>
+                <span>Terms Of Use</span>
+                <span>Privacy-Policy</span>
+                <span>FAQ</span>
+                <span>Watch List</span>
+            </h1>
             </div>
-        </footer>
-    );
-}
+            <div className='section__p'>
+            <p>©2023 WATCHIT. All Right Reserved. All videos and shows on this platform are trademarks of, and all related images and content are the property of, 
+            Streamit Inc. Duplication and copy of this is strictly prohibited. All rights reserved.</p>
+            </div>
+        </div>
+      
+        <div className='container__2'>
+            <div className='text'>
+                <p>Follow Us:</p>
+            </div>
+                                {/* Взял у ребят понравилось  */}
+            <div className='icons'>
+                <div className='circle'><FacebookOutlinedIcon sx={{width:'2rem', height: '2rem'}}/></div>
+                <div className='circle'><TwitterIcon sx={{width:'2rem', height: '2rem'}}/></div>
+                <div className='circle'><GoogleIcon sx={{width:'2rem', height: '2rem'}}/></div>
+                <div className='circle'><GitHubIcon sx={{width:'2rem', height: '2rem'}}/></div>
+            </div>
+        </div>
+                                {/* И вот это тоже  */}   
+        <div className='section_3'>
+            <div className='watch_app'>
+                <p>Watchit App</p>
+            </div>
+            <div className='get_it'>
+                <img className='images' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' alt='google play'></img>
+                <img className='images' src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/800px-Download_on_the_App_Store_Badge.svg.png' alt='app store'></img>
+            </div>
+        </div>
+     </div>   
+    )
+};
+
+export default Footer;
--- src/components/SinglCard/SinglCard.jsx
+++ src/components/SinglCard/SinglCard.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Card, CardMedia, Button, Typography } from '@mui/material';
 import '../../SinglCard.css'
 
-const SingleCard = ({ id, name, time, image, handleClick }) => {
+const SingleCard = ({ id, name,  image, time, handleClick }) => {
   return (
     <Card sx={{ width: 395, height: 222, position: 'relative', display:'flex', gap:40}}>
-      <CardMedia component="img" image={image} alt={name} />
+      <CardMedia component="img" image={image} alt={name}/>
       <div
         style={{
           position: 'reletive',
@@ -17,9 +17,14 @@ const SingleCard = ({ id, name, time, image, handleClick }) => {
             'linear-gradient(90deg, rgba(0, 0, 0, 0.80) 0%, rgba(20, 20, 20, 0.40) 50%, rgba(83, 100, 141, 0.00) 100%)',
         }}
       >
-        <Typography variant="h6" component="div" sx={{ position: 'absolute', bottom: 0, left: 0, padding: 2 ,color: '#fff'}}>
+        
+        <Typography variant="h6" component="div" sx={{ position: 'absolute', bottom: 0, left: 0, padding: 2 ,color: 'red' }}>
           {name}
+          <Typography variant="h6" component="div" sx={{color: 'white'}}>
+            {time}
+          </Typography>
         </Typography>
+        
         <Button variant="outlined" sx={{ position: 'absolute', bottom: 10, right: 10, border: '1px solid #E50914', background: '#E50914', color: '#fff' }} onClick={() => handleClick(id)}>Watch</Button>
       </div>
     </Card>
